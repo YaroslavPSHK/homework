84c1084bdf5a00587f601164e9ff9586f2262a94
YaroslavPSHK <yaroslav1443@gmail.com>
2025-05-26 11:26:08 +0300

    add: homework# 16

---
+++ lesson_16/js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const burger = document.querySelector(".header__burger");
+	const menu = document.querySelector(".header__menu");
+	const body = document.body;
+
+	burger.addEventListener("click", function () {
+		burger.classList.toggle("active");
+		menu.classList.toggle("active");
+		body.classList.toggle("lock");
+	});
+});
